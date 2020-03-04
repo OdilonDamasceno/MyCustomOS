2911593194e2fea92037ae40381bbd3502102bd9
Odilon Damasceno <storckskey@gmail.com>
2020-03-04 08:16:52 -0300

    Add memory segments and global descriptor table

--- kernel.cpp
+++ kernel.cpp
@@ -1,12 +1,12 @@
 #include "types.h"
+#include "gdt.h"
+
 void printf(char* str)
 {
     static uint16_t* VideoMemory = (uint16_t*)0xb8000;
 
     for (int i = 0; str[i] != '\0'; ++i)
-    {
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
-    }
     
 }
 
@@ -22,5 +22,6 @@ extern "C" void callConstructors()
 extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*magicnumber*/)
 {
     printf("Hello, world! Feito por Odilon Damasceno github.com/OdilonDamasceno");
+    GlobalDescriptorTable gdt;
     while (1);
 }
